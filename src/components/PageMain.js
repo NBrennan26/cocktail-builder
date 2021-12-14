ecd4de56814a215c7c75ef660dd9aff069ad875c
nbrennan26 <nbrennan26@gmail.com>
2021-12-14 15:09:02 -0600

    trying to get async fetch to work with react. Running into memory leaks

--- src/components/PageMain.js
+++ src/components/PageMain.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import { collection, getDocs, doc, setDoc } from "firebase/firestore";
-import { db } from "./Firestore";
-import uniqid from "uniqid";
+// import { collection, getDocs, doc, setDoc } from "firebase/firestore";
+// import { db } from "./Firestore";
+import axios from "axios";
+// import uniqid from "uniqid";
 import MyBar from "./MyBar";
 import MyCocktails from "./MyCocktails";
 import BuildBar from "./BuildBar";
@@ -11,6 +12,28 @@ import RandomDrink from "./RandomDrink";
 import About from "./About";
 
 function PageMain() {
+  const [testCocktail, setTestCocktail] = useState({});
+  const [testloading, setTestLoading] = useState(false);
+
+  useEffect(() => {
+    const loadCocktail = async () => {
+      setTestLoading(true);
+      const fetchData = await axios.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+      );
+      setTestCocktail(fetchData.data);
+      setTestLoading(false);
+    };
+    loadCocktail();
+  }, []);
+
+  useEffect(() => {
+    if (!testloading & testCocktail !== {}) {
+      console.log(testCocktail)
+      console.log(testCocktail.drinks[0].strDrink)
+    }
+  }, [testloading, testCocktail])
+
   return (
     <div id="page-main">
       <Routes>
