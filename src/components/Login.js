fa80e7d27c458a3b9e3435779934fc4e80981ab2
nbrennan26 <nbrennan26@gmail.com>
2021-12-16 10:05:54 -0600

    Make login a button rather than just text. Currently clicking just console.logs. Will add login logic with Firebase authentication later

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Login() {
-  return <div id="login">Login</div>;
+  return (
+    <div id="login">
+      <button id="login-btn" type="button" onClick={() => console.log("Click")}>Log in</button>
+    </div>
+  );
 }
 
 export default Login;
